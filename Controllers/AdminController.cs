b6e090524632e9a0f9fb85e1f1e3a2d4ef60af0c
Biggs <gilles.verga@gmail.com>
2020-05-21 15:46:26 +0200

    Add, Delete & Remove Guests

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Mariage.Data;
 using Mariage.Models;
+using Mariage.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -29,14 +30,58 @@ namespace Mariage.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddGuest(Participation model)
+        public async Task<IActionResult> UpdateGuest(int? id, EditGuestViewModel model)
         {
-            return View(model);
+            if (ModelState.IsValid)
+            {
+                var participation = new Participation(model.FirstName, model.LastName)
+                {
+                    IsInvitedToLunch = model.IsInvitedToLunch,
+                    CanBringChildren = model.CanBringChildren,
+                    CanBringPlusOne = model.CanBringPlusOne
+                };
+
+                if (!id.HasValue)
+                {
+                    _dbContext.Participations.Add(participation);
+                }
+                else
+                {
+                    participation.Id = id.Value;
+                    _dbContext.Participations.Update(participation);
+                }
+
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction("Guests");
+            }
+            return View("EditGuest", model);
+        }
+        public async Task<IActionResult> DeleteGuest(int id) 
+        {
+            var participation = await _dbContext.Participations.FindAsync(id);
+            _dbContext.Participations.Remove(participation);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("Guests");
+        }
+
+        public async Task<IActionResult> EditGuest(int id)
+        {
+            var participation = await _dbContext.Participations.FindAsync(id);
+            var model = new EditGuestViewModel
+            {
+                Id = participation.Id,
+                FirstName = participation.FirstName,
+                LastName = participation.LastName,
+                IsInvitedToLunch = participation.IsInvitedToLunch,
+                CanBringChildren = participation.CanBringChildren,
+                CanBringPlusOne = participation.CanBringPlusOne
+            };
+            return PartialView("EditGuest", model);
         }
 
         public IActionResult AddGuest()
         {
-            return PartialView();
+            return PartialView("EditGuest");
         }
     }
 }
--- wwwroot/script/main.js
+++ wwwroot/script/main.js
@@ -1,17 +1,63 @@
-window.get = function(url,callback)
-{
+window.get = function (url, callback) {
     httpRequest = new XMLHttpRequest();
     httpRequest.callback = callback;
-    httpRequest.onreadystatechange = callback;
+    httpRequest.onreadystatechange = function (event) {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            var response = event.currentTarget.responseText;
+            callback(response);
+        } else {
+            // Not ready yet.
+        }
+    }
     httpRequest.open('GET', url, true);
     httpRequest.send();
 }
 
-onRequestStatusChange = function(event)
-{
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        // Everything is good, the response was received.
-    } else {
-        // Not ready yet.
-    }
+window.onPopupLoaderClick = function (event) {
+    event.preventDefault();
+    var elem = event.currentTarget;
+    var url = elem.attributes["href"].value;
+    loadPopup(url);
 }
+
+window.loadPopup = function (url) {
+    window.get(url, window.applyPopupp);
+}
+
+window.onClosePopupClick = function(event) {
+ var target = event.currentTarget;
+ var popup = target.closest('.popup-container');
+ popup.remove();
+}
+
+window.applyPopupp = function (content) {
+    var popupcontainer = document.createElement("div");
+    popupcontainer.className = "popup-container";
+    document.body.append(popupcontainer);
+
+    var popup = document.createElement("div");
+    popup.className = "popup";
+    popupcontainer.append(popup);
+
+    var popupTop = document.createElement("div");
+    popupTop.className = "popup-top";
+    popup.append(popupTop);
+
+    var closeButton = document.createElement("span");
+    closeButton.className = "close-button";
+    closeButton.innerText = 'X';
+    closeButton.onclick = window.onClosePopupClick;
+    popupTop.append(closeButton);
+
+    var popupContent = document.createElement("div");
+    popupContent.className = "popup-content"; 
+    popupContent.innerHTML = content;
+    popup.append(popupContent);    
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var popups = document.getElementsByClassName("popup-loader");
+    for (var i = 0; i < popups.length; i++) {
+        popups[i].onclick = onPopupLoaderClick;
+    }
+});
